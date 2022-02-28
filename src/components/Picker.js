a482b11a8d41e92670c9cb4c92b54b5a4a8baa0a
Alysson Eduardo <54581169+Alysson1013@users.noreply.github.com>
2022-02-28 13:57:10 -0300

    Update Picker.js

--- src/components/Picker.js
+++ src/components/Picker.js
@@ -885,7 +885,7 @@ function Picker({
         <>
             {SelectedItemIconComponent}
             <Text style={_labelStyle} {...labelProps}>
-                {_selectedItemLabel.length > 14 ? _selectedItemLabel.substring(0, 14) + "..." : _selectedItemLabel}
+                {_selectedItemLabel.length > 9 ? _selectedItemLabel.substring(0, 9) + "..." : _selectedItemLabel}
             </Text>
         </>
     ), [SelectedItemIconComponent, _labelStyle, labelProps, _selectedItemLabel]);
